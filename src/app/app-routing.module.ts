3bb2fe2abfa9034a3ff062b2bfcdf404293a100b
Dang. Huynh Hai (MAR) <hhdang@tahl.com>
2023-11-17 13:39:28 +0700

    Add page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ShopPageComponent } from './pages/shop-page/shop-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "shop", component: ShopPageComponent},
+  {path: "cart", component: CartPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ShopPageComponent } from './pages/shop-page/shop-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ShopPageComponent,
+    CartPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
