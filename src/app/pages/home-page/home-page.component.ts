626e6924fd4be879e901955dba5f552aad5f7148
Dang. Huynh Hai (MAR) <hhdang@tahl.com>
2023-11-20 10:15:26 +0700

    Add home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
+  activeSlideIndex: number = 0;
 
+  public toPrevSlide = () => {
+    if (this.activeSlideIndex == 0) {
+      this.activeSlideIndex = 2;
+      return;
+    }
+    this.activeSlideIndex -= 1;
+  };
+
+  public toNextSlide = () => {
+    if (this.activeSlideIndex == 2) {
+      this.activeSlideIndex = 0;
+      return;
+    }
+    this.activeSlideIndex += 1;
+  };
+
+  public toSlide = (newSlideIndex: number) => {
+    this.activeSlideIndex = newSlideIndex;
+  };
 }
