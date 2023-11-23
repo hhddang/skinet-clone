758eda16fdd7832db67ff4801a076e29aa1df888
Dang. Huynh Hai (MAR) <hhdang@tahl.com>
2023-11-23 11:28:45 +0700

    Update home page: draggable image slider

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
   activeSlideIndex: number = 0;
+
+  ngOnInit() {
+    // Handle slide image on drag
+    const imageList = document.getElementsByClassName('image-list')[0];
+    var beginClientX = 0;
+    var endClientX = 0;
+
+    imageList.addEventListener('dragstart', (event) => {
+      beginClientX = (event as DragEvent).clientX;
+    });
+
+    imageList.addEventListener('dragend', (event) => {
+      endClientX = (event as DragEvent).clientX;
+      //  Slide whether drag distance is larger or equal to 200
+      if (beginClientX + 200 <= endClientX) {
+        this.toNextSlide();
+      }
+      if (endClientX + 200 <= beginClientX) {
+        this.toPrevSlide();
+      }
+    });
+  }
 
   public toPrevSlide = () => {
     if (this.activeSlideIndex == 0) {
